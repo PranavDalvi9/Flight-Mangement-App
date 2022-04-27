2a3780e60cb3a095870513405a196ce17aefb658
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-04-27 23:11:27 +0530

    search functionality added

--- Client/flight/src/Components/Home.jsx
+++ Client/flight/src/Components/Home.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 import Button from '@mui/material/Button';
+import axios from 'axios';
 
 export default function Home() {
 
     const [from, setFrom] = React.useState('');
     const [to, setTo] = React.useState('');
+    const[data, setData] = useState([])
+
+    useEffect(() =>{
+        axios.get("http://localhost:8080/Flights").then((res) => setData(res.data))
+    },[])
+
+    const handleSearch = () => {
+        const disp = data.filter((e) => {
+            // console.log("eee",e)
+            if((from === e.from) && (to === e.to)){
+                return e
+            }
+        })
+        console.log("disp",disp)
+    }
 
 
     return (
@@ -36,8 +52,12 @@ export default function Home() {
                         onChange={(e)=> {setTo(e.target.value)}}
                     /><br/>
 
-                    <Button variant="contained">Search</Button>
+                    <Button variant="contained" onClick={handleSearch}>Search</Button>
                 </Box>
+            </div>
+
+            <div>
+                
             </div>
         </div>
     )
