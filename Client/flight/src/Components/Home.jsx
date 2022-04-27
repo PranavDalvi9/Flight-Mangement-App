2b460d8f1c5ae3e6904282801eb778b6c7d01df5
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-04-27 21:47:38 +0530

    Home page added

--- Client/flight/src/Components/Home.jsx
+++ Client/flight/src/Components/Home.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+import Button from '@mui/material/Button';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>home</h1>
-    </div>
-  )
+
+    const [name, setName] = React.useState('');
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+        console.log(event.target.value)
+    };
+
+    return (
+        <div>
+            <div>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: 1, width: '85ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        id="outlined-name"
+                        label="FROM"
+                        // value={name}
+                        onChange={handleChange}
+                    /><br />
+
+                    <TextField
+                        id="outlined-name"
+                        label="TO"
+                        // value={name}
+                        onChange={handleChange}
+                    /><br/>
+
+                    <Button variant="contained">Search</Button>
+                </Box>
+            </div>
+        </div>
+    )
 }
