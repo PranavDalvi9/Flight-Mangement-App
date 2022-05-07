310856cb1e9995e71adc1253fa69c9d568c4e069
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-08 00:13:12 +0530

    done

--- src/configs/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-//   return mongoose.connect("mongodb+srv://rajarsi:1122@cluster0.j7xke.mongodb.net/petboarding");
-return mongoose.connect("mongodb+srv://rajarsi:1122@cluster0.j7xke.mongodb.net/myFlightDatabase?retryWrites=true&w=majority");
-};
